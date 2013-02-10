7ce81436264af008343e6c9b2b42d6d3371675a9
Victoria <v--victoria@mail.ru>
2013-02-10 14:01:03 +0400

    initial project version

---
+++ SemanticWebService/AppSemanticWebService/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render_to_response
+from classNode import Node
+
+def test(request):
+    """
+
+    :param request:
+    """
+    r = Node()
+    r.name = "root"
+    ch1 = Node()
+    ch2 = Node()
+    ch1.name = "ch1"
+    ch2.name = "ch2"
+    r.children.append(ch1)
+    r.children.append(ch2)
+    #r.children = []
+    r2 = Node()
+    r2.name = "root2"
+    #r2.children = []
+    roots = []
+    roots.append(r)
+    roots.append(r2)
+    return render_to_response('trees.html', {'roots': roots})
